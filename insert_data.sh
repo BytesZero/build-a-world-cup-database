4b994d0f1f8fdda8862102610a61c85aec64ae59
Zero <1300326388@qq.com>
2024-07-06 04:59:05 +0000

    🎉 完成插入数据脚本并插入数据

--- insert_data.sh
+++ insert_data.sh
@@ -8,3 +8,37 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+
+# 清空现有数据
+$($PSQL "TRUNCATE TABLE games, teams RESTART IDENTITY")
+
+# read csv
+cat games.csv | while IFS=',' read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  # 打印数据
+  echo $YEAR $ROUND $WINNER $OPPONENT $WINNER_GOALS $OPPONENT_GOALS
+  # 跳过标题行
+  if [[ $YEAR == "year" ]]
+  then
+    continue
+  fi
+  # 插入唯一的队伍名称
+  # 先插入胜方队伍
+  WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+  if [[ -z $WINNER_ID ]]
+  then
+    INSERT_WINNER_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
+    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+  fi
+  # 插入对手队伍名称
+  OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+  if [[ -z $OPPONENT_ID ]]
+  then
+    INSERT_OPPONENT_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
+    OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+  fi
+
+  # 插入比赛的数据
+  INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
+
+done
